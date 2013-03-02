b6af989b6dbe68dda6de71189143965ec1391e45
Emily Myers <emilyhmyers@gmail.com>
2013-03-02 18:30:35 -0500

    Revert "fixes most problems JSLint found"
    This reverts commit bfb9ef31acfd12317ca9540b8c801e209d1bceb0.

--- externals/changing_size.js
+++ externals/changing_size.js
@@ -1,27 +1,22 @@
-/* globals: $ */
-"use strict";
-var $ = window.$,
-    jQuery = window.jQuery;
-
-$(document).ready(function () {
-    window.onresize = function () {
-        // getting currrent size of animation
-        var currentWidth = document.getElementById("size").getAttribute("width");
-        var currentHeight = document.getElementById("size").getAttribute("height");
-        console.log("This is the current animation width: " + currentWidth);
-        console.log("This is the current animation height: " + currentHeight);
-        // getting current size of window
-        var windowWidth = $(document).width();
-        var windowHeight = $(document).height();
-        console.log("This is the current window width: " + windowWidth);
-        console.log("This is the current window height: " + windowHeight);
-        // setting new animation width
-        //document.getElementById("size").setAttribute("width", windowWidth);
-        document.getElementById("size").style.height = windowHeight - 100 + "px";
-        document.getElementById("size").setAttribute("height", windowHeight);
-        var newWindowWidth = document.getElementById("size").getAttribute("width");
-        var newWindowHeight = document.getElementById("size").getAttribute("height");
-        console.log("This is the new animation width: " + newWindowWidth);
-        console.log("This is the new animation width: " + newWindowHeight);
-    };
+$(document).ready(function() {
+	window.onresize = function() {
+		// getting currrent size of animation
+		var currentWidth = document.getElementById("size").getAttribute("width");
+		var currentHeight = document.getElementById("size").getAttribute("height");
+		console.log("This is the current animation width: " + currentWidth);
+		console.log("This is the current animation height: " + currentHeight);
+		// getting current size of window
+		var windowWidth = $(document).width();
+		var windowHeight = $(document).height();
+		console.log("This is the current window width: " + windowWidth);
+		console.log("This is the current window height: " + windowHeight);
+		// setting new animation width
+		//document.getElementById("size").setAttribute("width", windowWidth);
+		document.getElementById("size").style.height = windowHeight - 100 + "px";
+		document.getElementById("size").setAttribute("height", windowHeight);
+		var newWindowWidth = document.getElementById("size").getAttribute("width");
+		var newWindowHeight = document.getElementById("size").getAttribute("height");
+		console.log("This is the new animation width: " + newWindowWidth);
+		console.log("This is the new animation width: " + newWindowHeight);
+	}
 });
--- externals/spotlight.js
+++ externals/spotlight.js
@@ -1,66 +1,61 @@
-/* globals: $ */
-"use strict";
-var $ = window.$,
-    jQuery = window.jQuery;
+$(document).ready(function() {
+	$(".side_menu li img").click(function() {
+		// getting the alt tag
+		var id = $(this).attr("alt");
+		spotlightShow(id);
+	});
+
+	// on click, change active menu
+	$("nav li").click(function() {
+		$(".spotlight").children().hide();
+		$("#launchpad").fadeOut();
+		$(".spotlight").fadeIn();
+		
+		// swaps out menus
+		var active_menu = $(this).children("a").attr("data-menuid");
+		$(".side_menu").hide();
+		$(active_menu).fadeIn();
+
+		// displays the first item in the clicked section
+		var first_item = $(active_menu + " ul").children(":first-child"); 
+		first_item.find("img").click();
+	});
+
+	// when an image in the launchpad is clicked...
+	$("#launchpad img").click(function() {
+		var content = $(this).parent().attr("data-content");
+		var sidemenu = $("#" + content).attr("data-menu");
+		$("#" + sidemenu).fadeOut();
+		$("#" + sidemenu).addClass("active");
+		$("#launchpad").fadeOut();
+		$(".spotlight").fadeIn();
+		spotlightShow(content);
+	});
+
+	// fades everything out and the launchpad back in
+	$("#logo img").click(function() {
+		$(".spotlight").fadeOut();
+		$(".side_menu").hide();
+		$("#launchpad").fadeIn();
+	});
+
+	swffit.fit('size', 840, 520, 599, 371, false, false);
+
+});
 
 function showLightbox(id) {
-    var selector = '#' + id + '-lightbox';
-    console.log(selector);
-    $('.lightbox').hide();
-    $(selector).fadeIn();
+	var selector = '#' + id + '-lightbox';
+	console.log(selector);
+	$('.lightbox').hide();
+	$(selector).fadeIn();
 }
 
 function closeLightbox() {
-    $('.lightbox').fadeOut();
+	$('.lightbox').fadeOut();
 }
 
 function spotlightShow(id) {
-    console.log("Showing " + id);
-    $(".spotlight").children().hide();
-    $("#" + id).fadeIn();
+	console.log("Showing " + id);
+	$(".spotlight").children().hide();
+	$("#" + id).fadeIn();
 }
-
-$(document).ready(function () {
-    $(".side_menu li img").click(function () {
-        // getting the alt tag
-        var id = $(this).attr("alt");
-        spotlightShow(id);
-    });
-
-    // on click, change active menu
-    $("nav li").click(function () {
-        $(".spotlight").children().hide();
-        $("#launchpad").fadeOut();
-        $(".spotlight").fadeIn();
-        //
-        // swaps out menus
-        var active_menu = $(this).children("a").attr("data-menuid");
-        $(".side_menu").hide();
-        $(active_menu).fadeIn();
-        //
-        // displays the first item in the clicked section
-        var first_item = $(active_menu + "ul").children(":first-child"); 
-        first_item.find("img").click();
-    });
-
-    // when an image in the launchpad is clicked...
-    $("#launchpad img").click(function () {
-        var content = $(this).parent().attr("data-content");
-        var sidemenu = $("#" + content).attr("data-menu");
-        $("#" + sidemenu).fadeOut();
-        $("#" + sidemenu).addClass("active");
-        $("#launchpad").fadeOut();
-        $(".spotlight").fadeIn();
-        spotlightShow(content);
-    });
-
-    // fades everything out and the launchpad back in
-    $("#logo img").click(function () {
-        $(".spotlight").fadeOut();
-        $(".side_menu").hide();
-        $("#launchpad").fadeIn();
-    });
-
-    //swffit.fit('size', 840, 520, 599, 371, false, false);
-
-});
