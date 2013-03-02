1107128129cad48f7aad65229fb84623b14949e0
Emily Myers <emilyhmyers@gmail.com>
2013-03-02 18:51:59 -0500

    fixes all JSLint/HTML lint problems, validates code

--- externals/spotlight.js
+++ externals/spotlight.js
@@ -1,59 +1,68 @@
-$(document).ready(function() {
-	$(".side_menu li img").click(function() {
-		// getting the alt tag
-		var id = $(this).attr("alt");
-		spotlightShow(id);
-	});
-
-	// on click, change active menu
-	$("nav li").click(function() {
-		$(".spotlight").children().hide();
-		$("#launchpad").fadeOut();
-		$(".spotlight").fadeIn();
-		
-		// swaps out menus
-		var active_menu = $(this).children("a").attr("data-menuid");
-		$(".side_menu").hide();
-		$(active_menu).fadeIn();
-
-		// displays the first item in the clicked section
-		var first_item = $(active_menu + " ul").children(":first-child"); 
-		first_item.find("img").click();
-	});
-
-	// when an image in the launchpad is clicked...
-	$("#launchpad img").click(function() {
-		var content = $(this).parent().attr("data-content");
-		var sidemenu = $("#" + content).attr("data-menu");
-		$("#" + sidemenu).fadeOut();
-		$("#" + sidemenu).addClass("active");
-		$("#launchpad").fadeOut();
-		$(".spotlight").fadeIn();
-		spotlightShow(content);
-	});
-
-	// fades everything out and the launchpad back in
-	$("#logo img").click(function() {
-		$(".spotlight").fadeOut();
-		$(".side_menu").hide();
-		$("#launchpad").fadeIn();
-});
-
-});
+/* globals: $ */
+var $ = window.$,
+    jQuery = window.jQuery;
 
 function showLightbox(id) {
-	var selector = '#' + id + '-lightbox';
-	console.log(selector);
-	$('.lightbox').hide();
-	$(selector).fadeIn();
+    "use strict";
+    var selector = '#' + id + '-lightbox';
+    console.log(selector);
+    $('.lightbox').hide();
+    $(selector).fadeIn();
 }
 
 function closeLightbox() {
-	$('.lightbox').fadeOut();
+    "use strict";
+    $('.lightbox').fadeOut();
 }
 
 function spotlightShow(id) {
-	console.log("Showing " + id);
-	$(".spotlight").children().hide();
-	$("#" + id).fadeIn();
+    "use strict";
+    console.log("Showing " + id);
+    $(".spotlight").children().hide();
+    $("#" + id).fadeIn();
 }
+
+$(document).ready(function () {
+    "use strict";
+    $(".side_menu li img").click(function () {
+        // getting the alt tag
+        var id = $(this).attr("alt");
+        spotlightShow(id);
+    });
+
+    // on click, change active menu
+    $("nav li").click(function () {
+        var active_menu = $(this).children("a").attr("data-menuid"),
+            first_item = $(active_menu + " ul").children(":first-child");
+        //
+        $(".spotlight").children().hide();
+        $("#launchpad").fadeOut();
+        $(".spotlight").fadeIn();
+        //
+        // swaps out menus
+        $(".side_menu").hide();
+        $(active_menu).fadeIn();
+        //
+        // displays the first item in the clicked section
+        first_item.find("img").click();
+    });
+
+    // when an image in the launchpad is clicked...
+    $("#launchpad img").click(function () {
+        var content = $(this).parent().attr("data-content"),
+            sidemenu = $("#" + content).attr("data-menu");
+        $("#" + sidemenu).fadeOut();
+        $("#" + sidemenu).addClass("active");
+        $("#launchpad").fadeOut();
+        $(".spotlight").fadeIn();
+        spotlightShow(content);
+    });
+
+    // fades everything out and the launchpad back in
+    $("#logo img").click(function () {
+        $(".spotlight").fadeOut();
+        $(".side_menu").hide();
+        $("#launchpad").fadeIn();
+    });
+
+});
